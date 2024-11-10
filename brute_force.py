0e7717d74e9049097cfc3056f11435cb60de4230
lmannier <lmannier1337@gmail.com>
2024-11-10 15:15:24 +0100

    modification bruteforce

--- brute_force.py
+++ brute_force.py
@@ -1,60 +1,48 @@
-"""
-    Fonction BruteForce permettant d'avoir les meilleurs combinaisons
-    d'action pouvant être acheté tout en respectant certainne condition:
-    - Le montant maximum pouvant être mis est de 500 €
-
-    Voici la configuration des data:
-        Actions #  Coût par action (en euros) Bénéfice (après 2 ans)
-    0    Action-1                          20                     5%
-    1    Action-2                          30                    10%
-    ---
-
-    Nous constatons déja que le profit est correlé
-    au prix de l'action * les bénéfice après 2 ans.
-    Nous avons a disposition la formule suivante:
-        profit = coût de l'action * (1 + bénéfice en pourcentage)
-"""
-
-import pandas as pd
+import csv
 from itertools import combinations
+from typing import List
 
 
-def benefice_after_two_years(action_cost, increase_rate):
-    return action_cost * (1 + increase_rate)
+def calculate_profit(actions):
+    # Calcule uniquement le profit net sans inclure le coût initial
+    return sum(cost * (profit / 100) for _, cost, profit in actions)
 
 
-def best_combinaison(data, cost_limite):
+def getActions(file: str) -> List:
     actions = []
-
-    for index, action in data.iterrows():
-        action_name = action["Actions #"]
-        cost_per_action = action["Coût par action (en euros)"]
-        increase_rate = int(action["Bénéfice (après 2 ans)"]
-                            .replace("%", "")) / 100
-        future_benefice = benefice_after_two_years(
-            cost_per_action, increase_rate)
-
-        actions.append((action_name, cost_per_action, future_benefice))
-
-    best_profit = 0
-    best_combinaison_possible = []
-
-    for i in range(1, len(actions) + 1):
-        for combinaison in combinations(actions, i):
-            total_cost = sum(action[1] for action in combinaison)
-            total_profit = sum(action[2] for action in combinaison)
-
-            if total_cost <= COST_LIMITE and total_profit > best_profit:
-                best_profit = total_profit
-                best_combinaison_possible = combinaison
-
-    print("Action Name | Cost per action | increase after 2 years")
-    for row in best_combinaison_possible:
-        print(row)
-
-    print(f"Profit :  {best_profit}€")
+    with open(file, "r") as f:
+        action = csv.DictReader(f)
+        for item in action:
+            action_name = item["Actions #"]
+            action_cost = int(item["Coût par action (en euros)"])
+            profit_after_two_years = float(
+                item["Bénéfice (après 2 ans)"].replace("%", ""))
+            actions.append((action_name, action_cost, profit_after_two_years))
+    return actions
 
 
-actions = pd.read_csv("liste_action.csv")
-COST_LIMITE = 500
-best_combinaison(actions, COST_LIMITE)
+def createActionsCombination(data, limit=500):
+    max_profit = 0
+    min_cost = float('inf')  # Initialise le coût minimum à l'infini
+    best_investissement = []
+    n = len(data)
+
+    for i in range(1, n + 1):
+        for action_comb in combinations(data, i):
+            sum_actions_cost = sum(cost for _, cost, _ in action_comb)
+
+            if sum_actions_cost <= limit:
+                profit = calculate_profit(action_comb)
+                if profit > max_profit or (profit == max_profit and sum_actions_cost < min_cost):
+                    max_profit = profit
+                    min_cost = sum_actions_cost
+                    best_investissement = action_comb
+
+    print(f"Profit maximum: {max_profit}€, Coût: {min_cost}€")
+    for action in best_investissement:
+        print(f"{action[0]} - Coût: {action[1]}€ - Bénéfice: {action[2]}%")
+
+
+# Utilisation du programme avec un fichier CSV
+actions_list = getActions("liste_action.csv")
+createActionsCombination(actions_list)
